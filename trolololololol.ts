4a97159222bb4779f250635dedabe2377864d723
lucas-nguyen0 <lucas.nguyen@mths.ca>
2024-11-28 08:49:41 -0500

    Create trolololololol.ts

---
+++ trolololololol.ts
@@ -0,0 +1,8 @@
+/* Copyright (c) 2025 MTHS All rights reserved
+ *
+ * Created by: 🧌
+ * Created on: Octvember 2025
+ * This program is a trolololololololol
+*/
+
+basic.showString('You Got Trolled! 🧌🧌🧌🧌🧌🧌🧌🧌🧌🧌🧌🧌🧌')
